4225976966897153c553c0ef5d83583d71c5e0aa
Kirti Wagh <kirtiwagh09@gmail.com>
2023-10-14 21:50:49 +0530

    code updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,33 +1,34 @@
-# Basic Import
-import numpy as np
-import pandas as pd
-from src.exception import CustomException
-from src.logger import logging
-
-from src.utils import save_object
-from src.utils import evaluate_model
-from dataclasses import dataclass
-import sys
 import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd 
+import numpy as np
+from dataclasses import dataclass
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
+#from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
-@dataclass 
-class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
+from src.utils import save_object
+from src.utils import evaluate_model
+
+
+@dataclass
+class ModelTraningConfig:
+    train_model_file_obj = os.path.join("artifcats","model.pkl")
 
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_traner_config = ModelTraningConfig()
+
 
     def initate_model_training(self,train_array,test_array):
         try:
-            logging.info('Splitting Dependent and Independent variables from train and test data')
-            X_train, y_train, X_test, y_test = (
+            logging.info("Split Dependent And Independent Features")
+            X_train,y_train,X_test,y_test = (
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
@@ -36,8 +37,8 @@ class ModelTrainer:
 
             models = {
                 "Random Forest": RandomForestClassifier(),
-                "Decision Tree": DecisionTreeClassifier(),
-                "Logistic":LogisticRegression()
+                
+                "Logastic":LogisticRegression()
             }
             params = {
                 "Random Forest":{
@@ -46,16 +47,9 @@ class ModelTrainer:
                     'max_depth': [10, 8, 5],
                     'min_samples_split': [2, 5, 10],
                 },
-                "Decision Tree":{
-                    "class_weight":["balanced"],
-                    "criterion":['gini',"entropy"],
-                    "splitter":['best','random'],
-                    "max_depth":[3,4,5,6],
-                    "min_samples_split":[2,3,4,5],
-                    "min_samples_leaf":[1,2,3],
-                    "max_features":["sqrt","log2"]
-                },
-                "Logistic":{
+                
+            
+                "Logastic":{
                     "class_weight":["balanced"],
                     'penalty': ['l1', 'l2'],
                     'C': [0.001, 0.01, 0.1, 1, 10, 100],
@@ -63,31 +57,26 @@ class ModelTrainer:
                 }
             }
 
-            
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
-            print(model_report)
-            print('\n====================================================================================\n')
-            logging.info(f'Model Report : {model_report}')
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                                models=models,param=params)
 
-            # To get best model score from dictionary 
+                ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            
+
             best_model = models[best_model_name]
 
-            print(f'Best Model Found , Model Name : {best_model_name} ,  Accuracy_Score : {best_model_score}')
-            print('\n====================================================================================\n')
-            logging.info(f'Best Model Found , Model Name : {best_model_name} , Accuracy_Score : {best_model_score}')
+            print(f"Best Model Found, Model Name is: {best_model_name},Accuracy_Score: {best_model_score}")
+            print("\n***************************************************************************************\n")
+            logging.info(f"Best Model Found, Model Name is: {best_model_name},Accuracy_Score: {best_model_score}")
 
-            save_object(
-                 file_path=self.model_trainer_config.trained_model_file_path,
-                 obj=best_model
-            )
-          
+            save_object(file_path=self.model_traner_config.train_model_file_obj,
+                obj = best_model
+                )
 
         except Exception as e:
-            logging.info('Exception occured at Model Training')
+            logging.info("Error Occured in Model Traning")
             raise CustomException(e,sys)
